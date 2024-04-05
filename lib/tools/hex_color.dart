afb6f768d6bccb79a29d0fa7485b45eb72838e48
Rahmat Trinanda <rahmat3nanda@gmail.com>
2024-04-05 17:57:12 +0700

    create hex

---
+++ lib/tools/hex_color.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class HexColor extends Color {
+  HexColor(final String hexColor) : super(_getColorFromHex(hexColor));
+
+  static _getColorFromHex(String hexColor) {
+    hexColor = hexColor.toUpperCase().replaceAll("#", "");
+    if (hexColor.length == 6) {
+      hexColor = "FF$hexColor";
+      return int.parse(hexColor, radix: 16);
+    }
+    if (hexColor.length == 8) {
+      return Color(int.parse("0x$hexColor"));
+    }
+  }
+}
