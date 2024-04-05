13dcfdbbe3215cce5ff5fff9ba6a6a0b2af9bf5c
Rahmat Trinanda <rahmat3nanda@gmail.com>
2024-04-05 17:17:42 +0700

    config

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_news/app.dart';
@@ -6,6 +8,13 @@ import 'package:flutter_news/app.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  FlutterError.onError = (errorDetails) {
+    FirebaseCrashlytics.instance.recordFlutterFatalError(errorDetails);
+  };
+  PlatformDispatcher.instance.onError = (error, stack) {
+    FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+    return true;
+  };
   await SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
   ]);
